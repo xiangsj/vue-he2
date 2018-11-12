2d0db88c0e737acf5a9609f5ac31baf6644578a2
sh.james <378736618@qq.com>
2018-11-12 20:58:46 +0800

    pick city

--- src/pages/home/addCompany/selectCity.vue
+++ src/pages/home/addCompany/selectCity.vue
@@ -21,43 +21,63 @@ export default {
     //   popupVisible: true,
       currentObj: this.msg, // 当前选中项
       account: JSON.parse(this.$getCookie("account")),
+      slots__: [
+        // {
+        //   flex: 1,
+        //   values: [
+        //     "aaa-01",
+        //     "2015-02",
+        //     "2015-03",
+        //     "2015-04",
+        //     "2015-05",
+        //     "2015-06"
+        //   ],
+        //   className: "slot1",
+        //   textAlign: "center"
+        // },
+        // {
+        //   flex: 1,
+        //   values: [
+        //     "bbb-01",
+        //     "2015-02",
+        //     "2015-03",
+        //     "2015-04",
+        //     "2015-05",
+        //     "2015-06"
+        //   ],
+        //   className: "slot3",
+        //   textAlign: "center"
+        // },
+        // {
+        //   flex: 1,
+        //   values: [
+        //     "ccc-01",
+        //     "2015-02",
+        //     "2015-03",
+        //     "2015-04",
+        //     "2015-05",
+        //     "2015-06"
+        //   ],
+        //   className: "slot3",
+        //   textAlign: "center"
+        // }
+      ],
       slots: [
         {
           flex: 1,
-          values: [
-            "aaa-01",
-            "2015-02",
-            "2015-03",
-            "2015-04",
-            "2015-05",
-            "2015-06"
-          ],
+          values: [],
           className: "slot1",
           textAlign: "center"
         },
         {
           flex: 1,
-          values: [
-            "bbb-01",
-            "2015-02",
-            "2015-03",
-            "2015-04",
-            "2015-05",
-            "2015-06"
-          ],
+          values: [],
           className: "slot3",
           textAlign: "center"
         },
         {
           flex: 1,
-          values: [
-            "ccc-01",
-            "2015-02",
-            "2015-03",
-            "2015-04",
-            "2015-05",
-            "2015-06"
-          ],
+          values: [],
           className: "slot3",
           textAlign: "center"
         }
@@ -65,6 +85,7 @@ export default {
       pickArr: [],
       provinceDataArr: [],
       cityDataArr: [],
+      townDataArr: []
     };
   },
   created() {
@@ -72,13 +93,18 @@ export default {
   },
   methods: {
     onValuesChange(picker, values) {
-        console.log(picker.getSlotValue(0) + '------')
-        console.log(values[0] + ' ====== ')
+        // console.log(picker.getSlotValue(0) + '------')
+        // console.log(values[0] + ' ====== ')
         if (this.pickArr[0] != values[0]) {
-            console.log(' cccccc ')
+          console.log(' aaaaaa ')
+          this.pickArr = [].concat(values)
+          this.getCity() // 
         }
-        this.pickArr = values
-    //   console.log(picker);
+        if (this.pickArr[1] != values[1]) {
+          console.log(' bbbbbb ')
+          this.pickArr = [].concat(values)
+        }        
+      console.log(this.pickArr);
       console.log(values);
     //   this.slots[1].values = ["eee1", "eee2"];
       // picker.setSlotValue(1, 'llkkjj');
@@ -96,29 +122,47 @@ export default {
         this.slots[0].values = this.provinceDataArr.map(item => {
             return item.CityName
         })
-        console.log(this.provinceDataArr)
-        console.log(this.pickArr)
+        // console.log(this.provinceDataArr)
+        // console.log(this.pickArr)
 
-        // 以省ID 取市
+        
         // let provinceArr = this.provinceDataArr.filter(item => {
         //     return item.CityName == this.pickArr[0]
         // })
         // console.log(provinceArr)
         // // console.log(provinceObj[0].FID)
-        this.getCity(this.provinceDataArr[0])
+        this.getCity() // 以省ID 取市
     },
-    async getCity(provinceArr) {
+    async getCity() {
+      let provinceArr = this.provinceDataArr[0]
         let cityData = await this.$http.get("/api/GetCustChinaCity", {
-            params: { fid: this.account.fid, provinceId: provinceArr.FID || '375' }
+            params: { fid: this.account.fid, provinceId: provinceArr.FID }
         })
         this.cityDataArr = cityData.data.DataList
-        let CityName2 = this.cityDataArr.map(item => {
+        this.slots[1].values = this.cityDataArr.map(item => {
             return item.CityName
         })
-        this.slots[1].values = CityName2
 
         console.log(this.cityDataArr)
-        console.log(CityName2)
+        // console.log(CityName2)
+
+        this.getTown()
+    },
+    async getTown() {
+      let cityArr = this.cityDataArr[0]
+        let cityData = await this.$http.get("/api/GetCustChinaArea", {
+            params: { fid: this.account.fid, cityID: cityArr.FID }
+        })
+        // this.cityDataArr = cityData.data.DataList
+        // let CityName2 = this.cityDataArr.map(item => {
+        //     return item.CityName
+        // })
+        // this.slots[1].values = CityName2
+
+        // console.log(this.cityDataArr)
+        console.log(cityData)
+        console.log(cityData)
+        console.log(cityData)
     },
     open() {
       this.popupVisible = true;
